dba6c4404cbe0c8b90d7ad05c55893261a2aaf2f
minhnghia164 <73348282+minhnghia164@users.noreply.github.com>
2024-10-28 09:17:22 +0700

    page thankyou

--- wp-content/themes/hello-elementor-child/thankyou-page.php
+++ wp-content/themes/hello-elementor-child/thankyou-page.php
@@ -3,19 +3,65 @@
 /* Template Name: thanh toán custom */
 session_start();
 get_header();
+?>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/page-thankyou.css" type="text/css">
 
-if (isset($_SESSION['checkout_data'])) {
-    $checkout_data = $_SESSION['checkout_data'];
-    unset($_SESSION['checkout_data']); // Xóa dữ liệu sau khi sử dụng
-    ?>
-    <h2>Cảm ơn, <?php echo esc_html($checkout_data['name']); ?>!</h2>
-    <p>Thanh toán của bạn đã được xử lý thành công.</p>
-    <p>Phương thức thanh toán: <?php echo esc_html($checkout_data['payment_method']); ?></p>
-    <p>Loại SIM: <?php echo esc_html($checkout_data['sim_type'] == '1' ? 'eSIM' : 'SIM Vật lý'); ?></p>
-    <p>Chúng tôi sẽ liên lạc với bạn qua số điện thoại: <?php echo esc_html($checkout_data['phone']); ?></p>
-    <?php
-} else {
-    echo "<h2>Không có dữ liệu thanh toán.</h2>";
-}
+<div class="page-thankyou">
+    <div class="content">
+        <div class="ct-layer">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/image/Group-left.svg" alt="">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/image/Group-right.svg" alt="">
+        </div>
+        <div class="ct-head">
+            <div class="title">Quý khách đã thanh toán thành công!</div>
+            <div class="sub-title">Bạn đã thanh toán <?php echo wc_price(100000); ?> </div>
+        </div>
+        <div class="ct-body">
+            <div class="row-flex">
+                <span class="label">Mã đơn hàng</span>
+                <span class="value">292929292922929</span>
+            </div>
+            <div class="row-flex">
+                <span class="label">Trạng thái đơn hàng</span>
+                <span class="value tag-content">Đã thanh toán</span>
+            </div>
+            <div class="row-flex">
+                <span class="label">Người mua</span>
+                <span class="value">Hồng Ngọc</span>
+            </div>
+            <div class="row-flex">
+                <span class="label">Địa chỉ nhận hàng</span>
+                <span class="value">33 ngyễn an ninh, tương mai, Hà Nội</span>
+            </div>
+            <div class="row-flex">
+                <span class="label">Số đã đặt</span>
+                <span class="value name-item">0988 213 411</span>
+            </div>
+            <div class="row-flex">
+                <span class="label">Phương thức thanh toán</span>
+                <span class="value payment-method">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/assets/image/payment-cod.svg" alt="">
+                    COD
+                </span>
+            </div>
+            <div class="row-flex">
+                <span class="label">Số tiền</span>
+                <span class="value money"><?php echo wc_price(100000); ?></span>
+            </div>
+            <div class="row-flex">
+                <a href="#" class="btn-custom">Liên hệ</a>
+                <a href="/" class="btn-custom btn-primary">Trang chủ</a>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php
 
 get_footer();
+
+?>
